e49af52db43e57c7b4e56c6526bb2ce9d6c9a0e9
NotionWavelet <notionwavelet@gmail.com>
2024-05-17 03:18:47 +0200

    17 mayo

---
+++ js/footer.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Contenido del footer
+    var footerContent = `
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <div class="footer_social">
+                    <ul>
+                        <li><a href="https://twitter.com/NotionWavelet"><img src="images/twitter.png" alt="" width="35"></a></li>
+                        <li><a href="https://www.instagram.com/notionwavelet/"><img src="images/instagram.png" alt="" width="35"></a></li>
+                        <li><a href="https://www.tiktok.com/@notion.wavelet"><img src="images/tiktok.png" alt="" width="35"></a></li>
+                        <li><a href="https://www.pinterest.es/notionwavelet/"><img src="images/pinterest.png" alt="" width="35"></a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    &copy 2024 All rights reserved
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    // Selecciona el elemento donde quieres incluir el footer
+    var footerContainer = document.querySelector("#footer_container");
+
+    // Inserta el contenido del footer en el contenedor
+    if (footerContainer) {
+        footerContainer.innerHTML = footerContent;
+    }
+});
---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Contenido de la barra de navegación
+    var navbarContent = `
+    <div class="header_inner header_inner-expand-sm header_inner-custom d-flex flex-row align-items-center justify-content-start">
+			<nav class="main_nav">
+				<ul>
+					<li><img src="images/logo.png" alt="Notion Wavelet Logo" width="30"><a href="index.html">Notion Wavelet</a></li>
+					<li><a href="contact.html">Contacto</a></li>
+				</ul>
+			</nav>
+
+		</div>
+    `;
+
+
+    // Selecciona el elemento donde quieres incluir la barra de navegación
+    var navbarContainer = document.querySelector("#navbar_container");
+
+    // Inserta el contenido de la barra de navegación en el contenedor
+    if (navbarContainer) {
+        navbarContainer.innerHTML = navbarContent;
+    }
+
+
+});
